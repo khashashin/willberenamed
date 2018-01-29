2bd2f772ed5513b8142e9f447064478e324f15e6
khashashin <khashashin@gmail.com>
2018-01-29 16:36:59 +0100

    deployment fix

--- treichle_cup/settings/production.py
+++ treichle_cup/settings/production.py
@@ -1,3 +1,20 @@
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+from django.core.exceptions import ImproperlyConfigured
+# Handling Key Import Errors
+def get_env_variable(var_name):
+    """ Get the environment variable or return exception """
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = "Set the %s environment variable" % var_name
+        raise ImproperlyConfigured(error_msg)
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = get_env_variable('SECRET_KEY')
+
 from __future__ import absolute_import, unicode_literals
 
 from .base import *
