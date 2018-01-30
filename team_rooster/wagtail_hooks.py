26eedc54149a1301eb9798e23bc75b2791d5b497
khashashin <khashashin@gmail.com>
2018-01-30 22:35:41 +0100

    changes

--- team_rooster/wagtail_hooks.py
+++ team_rooster/wagtail_hooks.py
@@ -3,11 +3,20 @@
 from wagtail.contrib.modeladmin.options import (
     ModelAdmin, ModelAdminGroup, modeladmin_register)
 from . models import TeamRooster
+from news.models import NewsPage
 
 class TeamRoosterModelAdmin(ModelAdmin):
     model = TeamRooster
     menu_label = 'Team Manager'
     menu_icon = 'group'
     menu_order = 200  # will put in 3rd place (000 being 1st, 100 2nd)
-
+    
 modeladmin_register(TeamRoosterModelAdmin)
+
+class NewsModelAdmin(ModelAdmin):
+    model = NewsPage
+    menu_label = 'News'
+    menu_icon = 'edit'
+    menu_order = 300  # will put in 3rd place (000 being 1st, 100 2nd)
+
+modeladmin_register(NewsModelAdmin)
