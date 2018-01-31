1b167aa8b48123251b9a4017cc682ef01eca7a85
khashashin <khashashin@gmail.com>
2018-01-31 22:40:01 +0100

    added amazon s3 media cloud

--- treichle_cup/settings/base.py
+++ treichle_cup/settings/base.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
     'modelcluster',
     'taggit',
+    'storages',
 
     'django.contrib.admin',
     'django.contrib.auth',
--- treichle_cup/settings/production.py
+++ treichle_cup/settings/production.py
@@ -18,6 +18,13 @@ def get_env_variable(var_name):
         raise ImproperlyConfigured(error_msg)
 
 SECRET_KEY = get_env_variable('SECRET_KEY')
+AWS_STORAGE_BUCKET_NAME = get_env_variable('AWS_STORAGE_BUCKET_NAME')
+AWS_ACCESS_KEY_ID = get_env_variable('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = get_env_variable('AWS_SECRET_ACCESS_KEY')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+MEDIA_URL = 'https://%s/" % AWS_S3_CUSTOM_DOMAIN
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 try:
     from .local import *
