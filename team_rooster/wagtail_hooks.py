4841b21d04a27b9b40bc204496644ccee2dd219c
khashashin <khashashin@gmail.com>
2018-02-05 13:57:11 +0100

    fixed "jahrgang", added filters on admin side

--- team_rooster/wagtail_hooks.py
+++ team_rooster/wagtail_hooks.py
@@ -10,7 +10,8 @@ class TeamRoosterModelAdmin(ModelAdmin):
     menu_label = 'Team Manager'
     menu_icon = 'group'
     menu_order = 200  # will put in 3rd place (000 being 1st, 100 2nd)
-    
+    search_fields = ('title',)
+
 modeladmin_register(TeamRoosterModelAdmin)
 
 class NewsModelAdmin(ModelAdmin):
@@ -18,5 +19,8 @@ class NewsModelAdmin(ModelAdmin):
     menu_label = 'News'
     menu_icon = 'edit'
     menu_order = 300  # will put in 3rd place (000 being 1st, 100 2nd)
+    list_display = ('title', 'date_published', 'last_modified')
+    list_filter = ('date_published', 'last_modified')
+    search_fields = ('title',)
 
 modeladmin_register(NewsModelAdmin)
