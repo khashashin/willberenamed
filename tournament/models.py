3a941d690183debf0996a209d5bec192fc08a9b4
khashashin <khashashin@gmail.com>
2018-02-07 16:03:00 +0100

    added "poits" fields to tournament models

--- tournament/models.py
+++ tournament/models.py
@@ -4,6 +4,10 @@ from wagtail.wagtailcore.models import Page, Orderable
 from wagtail.wagtailadmin.edit_handlers import (
     FieldPanel,
     StreamFieldPanel,
+    MultiFieldPanel,
+    FieldRowPanel,
+    InlinePanel,
+    PageChooserPanel
 )
 from modelcluster.fields import ParentalKey
 from wagtail.wagtailcore.fields import StreamField
@@ -13,9 +17,8 @@ from wagtail.wagtailsnippets.models import register_snippet
 
 from team_rooster.models import TeamRooster
 
-class GoupstageTournamentRelationship(Orderable, models.Model):
-    page = ParentalKey('TournamentPage', related_name='groupstage_relationship')
-    number = models.PositiveSmallIntegerField(required=True,
+class GroupstageTournamentModel(models.Model):
+    number = models.PositiveSmallIntegerField(
         help_text="Add the unique number of this Match.")
     starts_at = models.DateTimeField()
     # Team 1
@@ -26,9 +29,14 @@ class GoupstageTournamentRelationship(Orderable, models.Model):
         related_name="+",
     )
     team_1_dress = ColorField(default='#ff0000', blank=True)
-    team_1_first_halftime_score = models.PositiveSmallIntegerField(blank=True)
-    team_1_second_halftime_score = models.PositiveSmallIntegerField(blank=True)
-    team_1_shootout_score = models.PositiveSmallIntegerField(blank=True)
+    team_1_first_halftime_score = models.PositiveSmallIntegerField(blank=True, default="")
+    team_1_first_halftime_point = models.PositiveSmallIntegerField(blank=True, default="")
+    team_1_second_halftime_score = models.PositiveSmallIntegerField(blank=True, default="")
+    team_1_second_halftime_point = models.PositiveSmallIntegerField(blank=True, default="")
+    team_1_shootout_score = models.PositiveSmallIntegerField(blank=True, default="")
+    team_1_shootout_point = models.PositiveSmallIntegerField(blank=True, default="")
+    team_1_total_score = models.PositiveSmallIntegerField(blank=True, default="")
+    team_1_total_points = models.PositiveSmallIntegerField(blank=True, default="")
     # Team 2
     team_2 = models.ForeignKey(
         TeamRooster,
@@ -37,9 +45,14 @@ class GoupstageTournamentRelationship(Orderable, models.Model):
         related_name="+",
     )
     team_2_dress = ColorField(default='#0066ff', blank=True)
-    team_2_first_halftime_score = models.PositiveSmallIntegerField(blank=True)
-    team_2_second_halftime_score = models.PositiveSmallIntegerField(blank=True)
-    team_2_shootout_score = models.PositiveSmallIntegerField(blank=True)
+    team_2_first_halftime_score = models.PositiveSmallIntegerField(blank=True, default="")
+    team_2_first_halftime_point = models.PositiveSmallIntegerField(blank=True, default="")
+    team_2_second_halftime_score = models.PositiveSmallIntegerField(blank=True, default="")
+    team_2_second_halftime_point = models.PositiveSmallIntegerField(blank=True, default="")
+    team_2_shootout_score = models.PositiveSmallIntegerField(blank=True, default="")
+    team_2_shootout_point = models.PositiveSmallIntegerField(blank=True, default="")
+    team_2_total_score = models.PositiveSmallIntegerField(blank=True, default="")
+    team_2_total_points = models.PositiveSmallIntegerField(blank=True, default="")
 
     panels = [
         FieldPanel('number', classname="6"),
@@ -47,17 +60,31 @@ class GoupstageTournamentRelationship(Orderable, models.Model):
         MultiFieldPanel([
             FieldRowPanel([
                 FieldPanel('team_1', classname="6"),
-                FieldPanel('team_1_color', classname="3"),
-                FieldPanel('team_1_score', classname="3"),
+                FieldPanel('team_1_dress', classname="6"),
+                FieldPanel('team_1_first_halftime_score', classname="3"),
+                FieldPanel('team_1_second_halftime_score', classname="3"),
+                FieldPanel('team_1_shootout_score', classname="3"),
+                FieldPanel('team_1_total_score', classname="3"),
+                FieldPanel('team_1_first_halftime_point', classname="3"),
+                FieldPanel('team_1_second_halftime_point', classname="3"),
+                FieldPanel('team_1_shootout_point', classname="3"),
+                FieldPanel('team_1_total_points', classname="3"),
             ]),
-        ], heading="Team 1"),
+        ], classname="full", heading="Team 1"),
         MultiFieldPanel([
             FieldRowPanel([
                 FieldPanel('team_2', classname="6"),
-                FieldPanel('team_2_color', classname="3"),
-                FieldPanel('team_2_score', classname="3"),
+                FieldPanel('team_2_dress', classname="6"),
+                FieldPanel('team_2_first_halftime_score', classname="3"),
+                FieldPanel('team_2_second_halftime_score', classname="3"),
+                FieldPanel('team_2_shootout_score', classname="3"),
+                FieldPanel('team_2_total_score', classname="3"),
+                FieldPanel('team_2_first_halftime_point', classname="3"),
+                FieldPanel('team_2_second_halftime_point', classname="3"),
+                FieldPanel('team_2_shootout_point', classname="3"),
+                FieldPanel('team_2_total_points', classname="3"),
             ]),
-        ], heading="Team 2"),
+        ], classname="full", heading="Team 2"),
     ]
 
     def __str__(self):
@@ -67,9 +94,25 @@ class GoupstageTournamentRelationship(Orderable, models.Model):
         verbose_name = 'Gruppenphase Spiel'
         verbose_name_plural = 'Gruppenphase'
 
-class FinalphaseTournamentRelationship(Orderable, models.Model):
-    page = ParentalKey('TournamentPage', related_name='final_phase_relationship')
-    number = models.PositiveSmallIntegerField(required=True,
+class GroupstageTournamentRelationship(Orderable, models.Model):
+    page = ParentalKey('TournamentPage',
+        related_name='groupstage_relationship')
+    match = models.ForeignKey('GroupstageTournamentModel',
+        related_name='match_tournament_relationship')
+    panels = [
+        PageChooserPanel('match')
+    ]
+class GroupstageScreencastRelationship(Orderable, models.Model):
+    page = ParentalKey('ScreencastPage',
+        related_name='groupstage_relationship')
+    match = models.ForeignKey('GroupstageTournamentModel',
+        default="", related_name='match_screen_relationship')
+    panels = [
+        PageChooserPanel('match')
+    ]
+
+class FinalphaseTournamentModel(models.Model):
+    number = models.PositiveSmallIntegerField(
         help_text="Add the unique number of this Match.")
     starts_at = models.DateTimeField()
     # Team 1
@@ -101,15 +144,19 @@ class FinalphaseTournamentRelationship(Orderable, models.Model):
         MultiFieldPanel([
             FieldRowPanel([
                 FieldPanel('team_1', classname="6"),
-                FieldPanel('team_1_color', classname="3"),
-                FieldPanel('team_1_score', classname="3"),
+                FieldPanel('team_1_dress', classname="3"),
+                FieldPanel('team_1_first_halftime_score', classname="3"),
+                FieldPanel('team_1_second_halftime_score', classname="3"),
+                FieldPanel('team_1_shootout_score', classname="3"),
             ]),
         ], heading="Team 1"),
         MultiFieldPanel([
             FieldRowPanel([
                 FieldPanel('team_2', classname="6"),
-                FieldPanel('team_2_color', classname="3"),
-                FieldPanel('team_2_score', classname="3"),
+                FieldPanel('team_2_dress', classname="3"),
+                FieldPanel('team_2_first_halftime_score', classname="3"),
+                FieldPanel('team_2_second_halftime_score', classname="3"),
+                FieldPanel('team_2_shootout_score', classname="3"),
             ]),
         ], heading="Team 2"),
     ]
@@ -120,6 +167,11 @@ class FinalphaseTournamentRelationship(Orderable, models.Model):
     class Meta:
         verbose_name = 'Finalphase Spiel'
         verbose_name_plural = 'Finalphase'
+
+class FinalphaseTournamentRelationship(Orderable, FinalphaseTournamentModel):
+    page = ParentalKey('TournamentPage', related_name='final_phase_relationship')
+class FinalphaseScreencastRelationship(Orderable, FinalphaseTournamentModel):
+    page = ParentalKey('ScreencastPage', related_name='final_phase_relationship')
 
 class TournamentPage(Page):
     starts_at = models.DateTimeField(blank=True)
@@ -164,7 +216,9 @@ class ScreencastPage(Page):
             panels=None, max_num=1),
     ]
 
+    parent_page_types = ['home.HomePage']
     subpage_types = []
 
     def __str__(self):
-        return self.title
+        # return self.title
+        return '{} \n Nächste: {}'.format(self.groupstage_relationship, self.final_phase_relationship)
