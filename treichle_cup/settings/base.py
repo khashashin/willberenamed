4c79daf7b1580c9638826fb91ba249aa4c21903c
khashashin <khashashin@gmail.com>
2018-02-14 12:17:49 +0100

    migrations bug fix

--- treichle_cup/settings/base.py
+++ treichle_cup/settings/base.py
@@ -107,8 +107,12 @@ DATABASES = {
 }
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'tcDB',
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#         'NAME': 'postgres',                      # Or path to database file if using sqlite3.
+#         'USER': 'postgres',                      # Not used with sqlite3.
+#         'PASSWORD': '******',                  # Not used with sqlite3.
+#         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+#         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
 #     }
 # }
 
